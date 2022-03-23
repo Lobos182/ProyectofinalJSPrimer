d0c56bb33c9577189430155ca9ed7849e381641b
lobos182 <lobos182@gmail.com>
2022-03-23 01:01:46 -0300

    workinggg

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,14 @@
 const carrito = [];
 
 const productos = [
-    { id: 1, titulo: "Zapa niky", precio: 900, categoria: "zapatillas deportivas", stock: 0, imagen: 'https://www.elalmanaque.com/Ene14/fotos/Teclado.jpg' },
-    { id: 2, titulo: "zapas adidas", precio: 850, categoria: "zapatillas deportivas", stock: 10, imagen: 'https://www.elalmanaque.com/Ene14/fotos/Teclado.jpg' },
-    { id: 3, titulo: "zapas puma", precio: 700, categoria: "zapatillas deportivas", stock: 5, imagen: 'https://www.elalmanaque.com/Ene14/fotos/Teclado.jpg' },
-    { id: 4, titulo: "pantalon niky", precio: 600, categoria: "ropa deportiva", stock: 56, imagen: '' },
-    { id: 5, titulo: "pantalon rebook", precio: 500, categoria: "ropa deportiva", stock: 0, imagen: 'https://www.elalmanaque.com/Ene14/fotos/Teclado.jpg' },
-    { id: 6, titulo: "short niky", precio: 450, categoria: "ropa deportiva", stock: 12, imagen: '' },
+    { id: 1, titulo: "Dinosaurio Flexible", precio: 900, categoria: "Animales", stock: 1, imagen: 'images/productos/dino1.jpg', imagen2: 'images/productos/dino2.jpg', imagen3: 'images/productos/dino3.jpg' },
+    { id: 2, titulo: "Estrella", precio: 850, categoria: "accesorios", stock: 10, imagen: 'images/productos/estrella1.jpg', imagen2: 'images/productos/estrella2.jpg', imagen3: 'images/productos/estrella3.jpg' },
+    { id: 3, titulo: "Pokebola", precio: 700, categoria: "organizadores", stock: 5, imagen: 'images/productos/pokebola1.jpg', imagen2: 'images/productos/pokebola2.jpg', imagen3: 'images/productos/pokebola3.jpg' },
+    { id: 4, titulo: "Pulpo flexible", precio: 600, categoria: "animales", stock: 56, imagen: 'images/productos/pulpo1.jpg', imagen2: 'images/productos/pulpo2.jpg' , imagen3: 'images/productos/pulpo3.jpg'  },
+    { id: 5, titulo: "Soporte Celular/Tablet", precio: 500, categoria: "accesorios", stock: 0, imagen: 'images/productos/soportecelu1.jpg', imagen2: 'images/productos/soportecelu2.jpg', imagen3: 'images/productos/soportecelu3.jpg' },
+    { id: 6, titulo: "Darth Vader", precio: 450, categoria: "muñecos", stock: 12, imagen: 'images/productos/vader1.jpg', imagen2: 'images/productos/vader2.jpg', imagen3: 'images/productos/vader3.jpg' },
+    { id: 7, titulo: "Otro", precio: 450, categoria: "muñecos", stock:0, imagen: '', imagen2: '', imagen3: '' },
+    { id: 8, titulo: "Otro", precio: 450, categoria: "muñecos", stock:0, imagen: '', imagen2: '', imagen3: '' },
 ];
 
 const agregarAlCarrito = (idProducto) => {
@@ -33,45 +35,44 @@ const agregarAlCarrito = (idProducto) => {
 
 
 generarCards(productos);
-//<img class="card-img-top" src="${elementoDelArray.imagen === '' ? 'https://aramar.com/wp-content/uploads/2017/05/aramar-suministros-para-el-vidrio-cristal-sin-imagen-disponible.jpg' : elementoDelArray.imagen}" alt="..." />
 function generarCards(productosAMostrar) {
     let acumuladorDeCards = ``;
     productosAMostrar.forEach((elementoDelArray) => {
         acumuladorDeCards += `<div class="col mb-5">
-        <div class="card h-100">
-            <!-- Sale badge-->
-            <div class="badge bg-dark text-white position-absolute" style="top: 0.5rem; right: 0.5rem">
-            ${elementoDelArray.stock > 0 ? 'Esta en venta' : 'out stock'}</div>
-            <!-- Product image-->            
-            <div id="carouselExampleIndicators" class="carousel slide" data-bs-ride="carousel">
+        <div class="card" style="width: 18rem;">
+            <!-- Ponemos si hay o no Stock-->
+            <div class="badge bg-dark badge-pill text-white position-absolute" style="top: 0.5rem; right: 0.5rem">
+            ${elementoDelArray.stock > 0 ? 'Hay Stock' : 'No Hay stock'}</div>
+            <!-- Carrousel de imagenes-->          
+            <div id="carouselExampleIndicators${elementoDelArray.id}" class="carousel slide" data-bs-ride="carousel">
             <div class="carousel-indicators">
-                <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active"
+                <button type="button" data-bs-target="#carouselExampleIndicators${elementoDelArray.id}" data-bs-slide-to="0" class="active"
                     aria-current="true" aria-label="Slide 1"></button>
-                <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1"
+                <button type="button" data-bs-target="#carouselExampleIndicators${elementoDelArray.id}" data-bs-slide-to="1"
                     aria-label="Slide 2"></button>
-                <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2"
+                <button type="button" data-bs-target="#carouselExampleIndicators${elementoDelArray.id}" data-bs-slide-to="2"
                     aria-label="Slide 3"></button>
             </div>
-            <div class="carousel-inner">
+            <div class="carousel-inner card-img-top">
                 <div class="carousel-item active">
-                    <img src="${elementoDelArray.imagen === '' ? 'https://aramar.com/wp-content/uploads/2017/05/aramar-suministros-para-el-vidrio-cristal-sin-imagen-disponible.jpg' : elementoDelArray.imagen}"
-                        class="d-block" alt="...">
+                    <img src="${elementoDelArray.imagen === '' ? 'images/productos/sinimagen.jpg' : elementoDelArray.imagen}"
+                        class="d-block w-100" alt="...">
                 </div>
                 <div class="carousel-item">
-                    <img src="${elementoDelArray.imagen === '' ? 'https://aramar.com/wp-content/uploads/2017/05/aramar-suministros-para-el-vidrio-cristal-sin-imagen-disponible.jpg' : elementoDelArray.imagen}"
-                        class="d-block" alt="...">
+                    <img src="${elementoDelArray.imagen2 === '' ? 'images/productos/sinimagen.jpg' : elementoDelArray.imagen2}"
+                        class="d-block w-100" alt="...">
                 </div>
                 <div class="carousel-item">
-                    <img src="${elementoDelArray.imagen === '' ? 'https://aramar.com/wp-content/uploads/2017/05/aramar-suministros-para-el-vidrio-cristal-sin-imagen-disponible.jpg' : elementoDelArray.imagen}"
-                        class="d-block" alt="...">
+                    <img src="${elementoDelArray.imagen3 === '' ? 'images/productos/sinimagen.jpg' : elementoDelArray.imagen3}"
+                        class="d-block w-100" alt="...">
                 </div>
             </div>
-            <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators"
+            <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators${elementoDelArray.id}"
                 data-bs-slide="prev">
                 <span class="carousel-control-prev-icon" aria-hidden="true"></span>
                 <span class="visually-hidden">Previous</span>
             </button>
-            <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators"
+            <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators${elementoDelArray.id}"
                 data-bs-slide="next">
                 <span class="carousel-control-next-icon" aria-hidden="true"></span>
                 <span class="visually-hidden">Next</span>
@@ -84,7 +85,7 @@ function generarCards(productosAMostrar) {
                     <h5 class="fw-bolder">${elementoDelArray.titulo}</h5>
                     <!-- Product price--> 
              <div class="input-group">
-                 <input class="form-control font-weight-bold" value="1" min="1" id="cantidad-${elementoDelArray.id}" type="number" placeholder="cantidad">
+                 <input ${elementoDelArray.stock > 0 ? '' : 'disabled'} class="form-control font-weight-bold" value="1" min="1" id="cantidad-${elementoDelArray.id}" type="number" placeholder="cantidad">
                 <div class="input-group-append">
                     <span class="input-group-text font-weight-bold">$${elementoDelArray.precio}</span>
                 </div>
@@ -96,7 +97,8 @@ function generarCards(productosAMostrar) {
             <div class="card-footer p-4 pt-0 border-top-0 bg-transparent">
                 <div class="text-center"><button 
                 onclick="agregarAlCarrito(${elementoDelArray.id})"
-                class="btn btn-outline-dark mt-auto" href="#">
+                class="btn btn-outline-dark mt-auto btn-info
+                ${elementoDelArray.stock > 0 ? '' : 'disabled'}"  href="#">
                 Agregar al Carrito
                  </button>
             </div>
@@ -113,6 +115,7 @@ function generarCards(productosAMostrar) {
 function mostrarCardsEnElHTML(cards) {
     document.getElementById("listado-productos").innerHTML = cards;
 };
+
 
 
 function suscribirseAlNewsletter() {
